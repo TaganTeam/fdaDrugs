47d3fcbfc780f4e0d1b312e32ee7a0f8601fc6c1
Artoym Pechoyrin <pecha7x@gmail.com>
2016-06-14 11:38:24 +0300

    Change project name

--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url      = 'api/v1/swagger_doc'
-GrapeSwaggerRails.options.app_name = 'spa_template'
+GrapeSwaggerRails.options.app_name = 'bshc_piv'
 GrapeSwaggerRails.options.app_url  = '/'
