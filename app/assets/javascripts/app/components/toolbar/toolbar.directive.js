86f9bdda47544f84a5e290eba592779b5faa4cca
Vyacheslav Kuzharov <marhuz.61@gmail.com>
2016-06-15 18:22:10 +0300

    Fix inject

--- app/assets/javascripts/app/components/toolbar/toolbar.directive.js
+++ app/assets/javascripts/app/components/toolbar/toolbar.directive.js
@@ -4,31 +4,25 @@
         .module('bshcPiv')
         .directive('toolbar', toolbar);
 
-    toolbarController.$inject = ['CONFIG', '$http', '$window'];
-
     function toolbar() {
         return {
             templateUrl: 'assets/app/components/toolbar/toolbar.tpl.html',
             controller: toolbarController,
             controllerAs: 'toolbarCtrl'
         };
+    }
 
-        
+    toolbarController.$inject = ['CONFIG', '$http', '$window'];
 
-        function toolbarController(CONFIG, $http, $window) {
-            var vm = this;
-            vm.logout = logout;
+    function toolbarController(CONFIG, $http, $window) {
+        var vm = this;
+        vm.logout = logout;
 
-            function logout() {
-                $http.delete(CONFIG.APIProductionHost + '/api/v1/logout').then(function (response) {
-                    if(response.data.code === 200)
-                        $window.location.reload();
-                })
-            }
-
-            
-
-
+        function logout() {
+            $http.delete(CONFIG.APIProductionHost + '/api/v1/logout').then(function (response) {
+                if(response.data.code === 200)
+                    $window.location.reload();
+            })
         }
     }
 })();
