eff645c6f6f9b868775cc4663720859cbd9ed038
Vyachaslav Kuzharov <marhuz.61@gmail.com>
2016-06-19 15:14:01 +0300

    Some changes. Need fix parse New Drug

--- app/models/drug_application.rb
+++ app/models/drug_application.rb
@@ -2,4 +2,7 @@ class DrugApplication < ActiveRecord::Base
 
   has_many :app_products
   has_one :drug
+
+
+  validates :application_number, presence: true, uniqueness: true
 end
--- lib/scraper/drug_details.rb
+++ lib/scraper/drug_details.rb
@@ -13,18 +13,16 @@ module Scraper
 
         drug_details = get_drug_details(drug_table)
         drug = save_or_find_drug(drug_details)
-        save_drug_app(app, drug_details, drug.id)
 
+        save_drug_app(app, drug_details, drug.id)
         save_products(app, get_products_details(products_table, app.application_number))
       end
     end
 
     def get_drug_details_page appl_no
-      mechanize = Mechanize.new
-      page = mechanize.get('https://www.accessdata.fda.gov/scripts/cder/drugsatfda/index.cfm')
+      page = get_data_page('https://www.accessdata.fda.gov/scripts/cder/drugsatfda/index.cfm')
 
       form = page.form('displaysearch')
-      p appl_no
       form['searchTerm'] = appl_no
       new_page = form.submit
 
@@ -49,55 +47,8 @@ module Scraper
       table
     end
 
-    def get_drug_details data_table
-      search_results = {}
 
-      data_table.search('tr').each do |row|
-        key = row.search('td.details_table')[0].text.downcase.gsub(/\s+/, '') rescue ''
-        val = row.search('td.details_table')[1].text rescue ''
 
-        search_results[:brand_name] = val if key == 'drugname(s)'
-        search_results[:application_number] = val if key == 'fdaapplicationno.'
-        search_results[:generic_name] = val if key == 'activeingredient(s)'
-        search_results[:company] = val if key == 'company'
-        search_results[:approval_date] = val if key == 'originalapprovalortentativeapprovaldate'
-      end
-      search_results
-    end
-
-    def get_products_details products_table, appl_no
-      ary = []
-      products_table.search('tr').each_with_index do |row, i|
-        products = {}
-
-        row.search('td.product_table').each_with_index do |item, index|
-          case index
-            when 2
-              products[:strength] = item.text
-            when 3
-              products[:dosage] = item.text
-            when 4
-              products[:market_status] = item.text
-          end
-        end
-        products[:product_number] = get_product_number(i, appl_no)
-        ary << products
-      end
-      ary.shift
-      ary
-    end
-
-    def get_product_number index, appl_no
-      ary = []
-      File.open("lib/scraper/data_files/Product.txt") do |f|
-       f.each_line do |line|
-          if line.match(/#{appl_no}/)
-             ary << line.gsub(/\r|\n|\t/,'')[6..8]
-           end
-        end
-      end
-      ary[index-1]
-    end
 
 
     private
