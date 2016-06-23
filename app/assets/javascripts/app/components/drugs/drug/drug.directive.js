6f56bd4ef6fdb89cf210870abc3869ad3fe22a50
Artoym Pechoyrin <pecha7x@gmail.com>
2016-06-24 01:16:02 +0300

    change assets path

--- app/assets/javascripts/app/components/drugs/drug/drug.directive.js
+++ app/assets/javascripts/app/components/drugs/drug/drug.directive.js
@@ -7,7 +7,7 @@
 
     function drug() {
         return {
-            templateUrl: '/components/drugs/drug/drug.tpl.html',
+            templateUrl: 'drug.tpl.html',
             scope: {
               drugs: '='  
             },
