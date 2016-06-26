21f8945bbf49eef04176bc3d04ba3608ec4fd7ee
Artoym Pechoyrin <pecha7x@gmail.com>
2016-06-26 11:16:10 +0300

    tmp

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -8,12 +8,6 @@ class Drug < ActiveRecord::Base
   api_accessible :basic do |t|
     t.add :id
     t.add :brand_name
-    t.add :has_patent
     t.add lambda{ |drug| drug.drug_applications.as_api_response(:light_status) rescue nil }, as: :apps
-  end
-
-
-  def has_patent
-    drug_applications.joins(:app_products).where("app_products.category_id = ?", self.company.id)
   end
 end
