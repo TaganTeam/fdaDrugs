3dbea80ca2d1a485309e086bc268670e1e3c4833
Artoym Pechoyrin <pecha7x@gmail.com>
2016-06-26 10:45:21 +0300

    add forgotten migration

--- db/migrate/20160625190640_create_schedulers.rb
+++ db/migrate/20160625190640_create_schedulers.rb
@@ -1,12 +1,15 @@
 class CreateSchedulers < ActiveRecord::Migration
   def change
-    create_table :schedulers do |t|
-      t.string   "type",                           null: false
-      t.text     "task",                           null: false
-      t.time     "time"
-      t.string   "day"
-      t.string   "frequency"
-      t.datetime "deleted_at"
+    create_table :delayed_jobs do |t|
+      t.integer  "priority",     default: 0, null: false
+      t.integer  "attempts",     default: 0, null: false
+      t.text     "handler",                  null: false
+      t.text     "last_error"
+      t.datetime "run_at"
+      t.datetime "locked_at"
+      t.datetime "failed_at"
+      t.string   "locked_by"
+      t.string   "queue"
 
       t.timestamps null: false
     end
