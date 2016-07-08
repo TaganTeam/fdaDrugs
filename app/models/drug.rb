595682e84f8ad611e2c1a17cf8521724d333397e
Vyacheslav Kuzharov <marhuz.61@gmail.com>
2016-07-08 13:14:05 +0300

    test

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -7,8 +7,8 @@ class Drug < ActiveRecord::Base
   has_many :app_products, through: :drug_applications
 
   api_accessible :basic do |t|
-    # t.add :id
-    # t.add :brand_name
-    # t.add lambda{ |drug| drug.drug_applications.as_api_response(:light_status) rescue nil }, as: :apps
+    t.add :id
+    t.add :brand_name
+    t.add lambda{ |drug| drug.drug_applications.as_api_response(:light_status) rescue nil }, as: :apps
   end
 end
