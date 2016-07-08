55f136f0d3ba139ad4626dacf90d89e50eb40480
Vyacheslav Kuzharov <marhuz.61@gmail.com>
2016-07-08 13:06:35 +0300

    test

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -4,10 +4,11 @@ class Drug < ActiveRecord::Base
   audited
 
   has_many :drug_applications
+  has_many :app_products, through: :drug_applications
 
   api_accessible :basic do |t|
-    t.add :id
-    t.add :brand_name
+    # t.add :id
+    # t.add :brand_name
     # t.add lambda{ |drug| drug.drug_applications.as_api_response(:light_status) rescue nil }, as: :apps
   end
 end
