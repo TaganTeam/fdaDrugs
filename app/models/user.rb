a354dac9fa16c40bda6ca112b1566b45ad44b86c
Vyacheslav Kuzharov <marhuz.61@gmail.com>
2016-07-08 13:40:34 +0300

    Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   acts_as_api
-  audited
+  # audited
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :lockable, :timeoutable
